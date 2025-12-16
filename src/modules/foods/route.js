6c3c3e1bb342b8b54f0b987f2361ce0f5ea7ea9e
Emeka Destiny <emekadestiny9090@gmail.com>
2025-12-16 03:46:35 -0800

    chore: deploy to server

--- src/modules/foods/route.js
+++ src/modules/foods/route.js
@@ -1,12 +1,26 @@
 import { Router } from "express";
 import foodController from "./controllers.js";
+import {
+  authMiddleware,
+  ownerMiddleware,
+} from "../../middleware/authMiddleware.js";
 const foodRouter = Router();
 
-foodRouter.post("/add-food", foodController.addFood);
+foodRouter.post("/add-food", authMiddleware, foodController.addFood);
 foodRouter.get("/all", foodController.getAll);
 foodRouter.get("/all/:restaurantId", foodController.getAllRestaurantFood);
 foodRouter.get("/:foodId", foodController.getOne);
-foodRouter.patch("/:foodId", foodController.updateOne);
-foodRouter.delete("/:foodId", foodController.deleteOne);
+foodRouter.patch(
+  "/:foodId",
+  authMiddleware,
+  ownerMiddleware,
+  foodController.updateOne
+);
+foodRouter.delete(
+  "/:foodId",
+  authMiddleware,
+  ownerMiddleware,
+  foodController.deleteOne
+);
 
 export default foodRouter;
--- src/modules/users/route.js
+++ src/modules/users/route.js
@@ -1,13 +1,17 @@
 import { Router } from "express";
 import userController from "./controllers.js";
+import {
+  adminMiddleware,
+  ownerMiddleware,
+} from "../../middleware/authMiddleware.js";
 const userRouter = Router();
 
-userRouter.get("/all", userController.getAll);
-userRouter.get("/session", userController.getSession);
+userRouter.get("/all", adminMiddleware, userController.getAll);
+userRouter.get("/session", ownerMiddleware, userController.getSession);
 userRouter.get("/:userId", userController.getOne);
-userRouter.patch("/session", userController.updateSession);
-userRouter.patch("/:userId", userController.updateOne);
-userRouter.delete("/session", userController.deleteSession);
-userRouter.delete("/:userId", userController.deleteOne);
+userRouter.patch("/session", ownerMiddleware, userController.updateSession);
+userRouter.patch("/:userId", ownerMiddleware, userController.updateOne);
+userRouter.delete("/session", ownerMiddleware, userController.deleteSession);
+userRouter.delete("/:userId", ownerMiddleware, userController.deleteOne);
 
 export default userRouter;
